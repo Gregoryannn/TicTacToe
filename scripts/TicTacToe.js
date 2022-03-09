121e52a10f0e8f74f4b4c59b4c6b31c0370d0b62
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-03-09 15:10:25 +0000

    Optymize game code

--- scripts/TicTacToe.js
+++ scripts/TicTacToe.js
@@ -1,8 +1,5 @@
 const PLAYER_ONE = 'round';
 const PLAYER_TWO = 'cross';
-
-
-
 class Game {
     cells = [...document.querySelectorAll('.game__field')]
     currentGameCombination = [
@@ -10,18 +7,16 @@ class Game {
         ['', '', ''],
         ['', '', ''],
     ]
-
-
     winGameCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]
     ]
 
 
     round = 1;
-    maxRounds = 9;
     row = null;
     column = null;
     isWin = false;
+
 
 
     selectCell(cell) {
@@ -31,67 +26,59 @@ class Game {
         row = cell.getAttribute('data-row') - 1;
         column = cell.getAttribute('data-column') - 1;
 
-
-        if (this.round % 2 === 0) {
-            player = PLAYER_ONE
-        } else {
-            player = PLAYER_TWO
-        }
+        this.round % 2 === 0 ? player = PLAYER_ONE : player = PLAYER_TWO;
 
         if (this.currentGameCombination[row][column]) return;
 
         cell.classList.add(player)
         this.currentGameCombination[row][column] = player;
-        if (this.round !== 9) this.round++
-        this.checkPosition(player)
-
+        this.addPlayerPosition(player)
     }
 
 
-    checkPosition(player) {
-        let arr = [].concat(...this.currentGameCombination);
-        let indexes = [];
-        let i = -1;
-        while ((i = arr.indexOf(player, i + 1)) != -1) {
-            indexes.push(i);
-        }
-        if (indexes.length >= 3) {
-            this.checkCombinations(indexes)
+       
+        addPlayerPosition(player){
+            this.round++
+            let arr = [].concat(...this.currentGameCombination);
+            let palyerPositions = [];
+            let i = -1;
+            while ((i = arr.indexOf(player, i + 1)) != -1) {
+
+                palyerPositions.push(i);
+            }
+            if (palyerPositions.length >= 3) this.checkCombinations(palyerPositions)
             if (this.isWin) this.won()
         }
+
+
+            checkCombinations(palyerPositions){
+                if (this.round > 9 && !this.isWin) alert('Draw')
+
+                const winCombinations = this.winGameCombinations;
+                let compatibility = 0;
+
+                    for (let combination of winCombinations) {
+                        compatibility = 0;
+                        palyerPositions.forEach(position => {
+                                if (combination.includes(position)) compatibility++;
+                                if (compatibility === 3) this.isWin = true;
+                            })
+                        }
     }
 
+                    won(){
+                        console.log('wygrana');
+                        // this.cells.forEach(cell => cell.removeEventListener('click', () => this.selectCell(cell)))
+                    }
 
-    checkCombinations(indexes) {
-        const winComb = this.winGameCombinations;
-        let compatibility = 0;
-
-        for (let array of winComb) {
-            compatibility = 0;
-            indexes.forEach(cell => {
-                if (array.includes(cell)) compatibility++;
-                if (compatibility === 3) this.isWin = true;
-            })
-        }
-
-    }
-
-
-    won() {
-        console.log('wygrana');
-    }
-
-    addListenersOnElements() {
-        this.cells.forEach(cell => cell.addEventListener('click', () => this.selectCell(cell)))
-    }
-    initGame() {
-        this.addListenersOnElements();
-    }
-}
-
-
-
-window.onload = function () {
-    const game = new Game();
-    game.initGame()
-}
+                    addListenersOnElements(){
+                        this.cells.forEach(cell => cell.addEventListener('click', () => this.selectCell(cell)))
+                    }
+                    initGame(){
+                        this.addListenersOnElements();
+                    }
+                }
+                window.onload = function () {
+                    const game = new Game();
+                    game.initGame()
+                }
