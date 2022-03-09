81e1019f2562a4be351670ba225ab5c05abc1a25
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-03-09 14:27:46 +0000

    Add current combination

--- scripts/TicTacToe.js
+++ scripts/TicTacToe.js
@@ -1,13 +1,47 @@
+const PLAYER_ONE = 'round';
+const PLAYER_TWO = 'cross';
+
+
+
 class Game {
     cells = [...document.querySelectorAll('.game__field')]
-    activeCells = cells.length
+    currentGameCombination = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ]
+    round = 1;
+    maxRounds = 9;
+    row = null;
+    column = null;
+
+    selectCell(cell) {
+        let player;
+        let row = this.row;
+        let column = this.column;
+        row = cell.getAttribute('data-row') - 1;
+        column = cell.getAttribute('data-column') - 1;
+
+        if (this.round % 2 === 0) {
+            player = PLAYER_ONE
+        } else {
+            player = PLAYER_TWO
+        }
+
+        if (this.currentGameCombination[row][column]) return;
+
+        cell.classList.add(player)
+        this.currentGameCombination[row][column] = player;
+        console.log(this.currentGameCombination)
+        this.round++;
+    }
 
     addListenersOnElements() {
-        this.cells.forEach(cell => cell.addEventListener('click', selectCell()))
+        this.cells.forEach(cell => cell.addEventListener('click', () => this.selectCell(cell)))
     }
 
     initGame() {
-        addListenersOnElements();
+        this.addListenersOnElements();
     }
 }
 
